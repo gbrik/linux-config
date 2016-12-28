c12a2a82ff409836576cb9b98a487afb89418a4a
Garo Brik <garo.brik@gmail.com>
2016-12-28 03:43:05 -0500

    add wal as a subtree

--- wm/wall.sh
+++ wm/wall.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
+SCRIPTPATH=$(dirname $(readlink -f "$0"))
+
 if type -p pacman >/dev/null; then
     sudo pacman -Sy --asexplicit feh imagemagick
 else
-    echo "install feh"
+    echo "install feh and imagemagick"
 fi
 
 mkdir -p "$HOME/scripts"
 
-git clone https://github.com/dylanaraps/wal "$HOME/scripts/wal"
+ln -s "$SCRIPTPATH/wal/wal" "$HOME/scripts/wal"
