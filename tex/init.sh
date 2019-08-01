905463b26a92e3788db1678e629fec6bd0bfe356
Garo Brik <garo.brik@gmail.com>
2019-07-31 22:00:51 -0400

    add tex config

--- tex/init.sh
+++ tex/init.sh
@@ -3,7 +3,9 @@
 SCRIPTPATH=$(dirname $(readlink -f "$0"))
 
 if type -p pacman >/dev/null; then
-    sudo pacman -Sy texlive-most
+    sudo pacman -Sy texlive-most ghostscript pygmentize
 else
     echo "No pacman, install packages from $SCRIPTPATH/tex.sh some other way."
 fi
+
+ln -s "$SCRIPTPATH/.latexmkrc" "$HOME/.latexmkrc"
