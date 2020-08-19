baf2f590f7ee058958c9964bcc90ff0c041b25dc
arrow <arrow@pop-os.localdomain>
2020-08-18 20:29:43 -0400

    update i3-gaps init script

--- i3-gaps/init.sh
+++ i3-gaps/init.sh
@@ -2,10 +2,10 @@
 
 SCRIPTPATH=$(dirname $(readlink -f "$0"))
 
-if type -p apt >/dev/null; then
+if command -v apt &> /dev/null; then
     sudo add-apt-repository ppa:kgilmer/speed-ricer
     sudo apt update
-    sudo apt install i3-wm feh compton undistract-me playerctl zathura
+    sudo apt install i3-gaps-wm feh compton undistract-me playerctl zathura
 
     sudo apt install gobject-introspection libgtk-3-doc libglib2.0-0
     wget http://ftp.nl.debian.org/debian/pool/main/p/playerctl/libplayerctl2_2.0.1-1_amd64.deb
@@ -18,5 +18,6 @@ fi
 
 CONFIGPATH="$HOME/.config"
 mkdir -p "$CONFIGPATH"
-rm -r "$CONFIGPATH/i3"
+rm -rf "$CONFIGPATH/i3"
 ln -s "$SCRIPTPATH/i3" "$CONFIGPATH/i3"
+ln -s "$SCRIPTPATH/compton.conf" "$CONFIGPATH/compton.conf"
