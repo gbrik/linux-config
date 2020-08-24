b3ad0e693133129b51a09154e778554e62a88478
Garo Brik <garo.brik@gmail.com>
2020-08-24 17:44:44 -0400

    update i3-gaps init script

--- i3-gaps/init.sh
+++ i3-gaps/init.sh
@@ -5,13 +5,24 @@ SCRIPTPATH=$(dirname $(readlink -f "$0"))
 if command -v apt > /dev/null; then
     sudo add-apt-repository ppa:kgilmer/speed-ricer
     sudo apt update
-    sudo apt install i3-gaps-wm feh compton undistract-me playerctl zathura
+    sudo apt install \
+	    i3-gaps-wm \
+	    feh compton \
+	    undistract-me \
+	    playerctl \
+	    zathura \
+	    gobject-introspection \
+	    libgtk-3-doc \
+	    libglib2.0-0 \
+	    cargo \
+	    libdbus-1-dev
 
-    sudo apt install gobject-introspection libgtk-3-doc libglib2.0-0
     wget http://ftp.nl.debian.org/debian/pool/main/p/playerctl/libplayerctl2_2.0.1-1_amd64.deb
     wget http://ftp.nl.debian.org/debian/pool/main/p/playerctl/playerctl_2.0.1-1_amd64.deb
     sudo dpkg -i libplayerctl2_2.0.1-1_amd64.deb playerctl_2.0.1-1_amd64.deb
     rm libplayerctl2_2.0.1-1_amd64.deb playerctl_2.0.1-1_amd64.deb
+
+    cargo install --git https://github.com/greshake/i3status-rust i3status-rs
 else
     echo "Couldn't install packages"
 fi
