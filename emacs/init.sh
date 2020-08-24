c24da73a3bab1bd2e626e502f5ef1d9af94f1958
Garo Brik <garo.brik@gmail.com>
2020-08-24 17:45:16 -0400

    update emacs init script

--- emacs/init.sh
+++ emacs/init.sh
@@ -2,19 +2,16 @@
 
 SCRIPTPATH=$(dirname $(readlink -f "$0"))
 
-if type -p pacman >/dev/null; then
+if command -v pacman > /dev/null; then
     sudo pacman -Sy emacs ttf-symbola xclip xsel
-elif type -p apt >/dev/null; then
-    sudo apt install emacs25 fonts-symbola xclip xsel
+elif command -v apt > /dev/null; then
+    sudo apt install emacs25 xclip xsel
 else
     echo "Couldn't install packages"
 fi
 
 git clone https://github.com/syl20bnr/spacemacs "$HOME/.emacs.d"
 
-rm -r "$HOME/.emacs.d/private"
-
-ln -s "$SCRIPTPATH/spacemacs-config" "$HOME/.emacs.d/private"
 ln -s "$HOME/.emacs.d/private/.spacemacs" "$HOME/.spacemacs"
 
 mkdir -p "$HOME/.config/systemd/user"
